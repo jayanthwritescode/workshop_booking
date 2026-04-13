7da7a787c8c5c51d2191cc50ddaf6c8c1b0d70dd
Jayanth <jayanthneelaiahgari@gmail.com>
2026-04-13 23:02:39 +0530

    fix: resolve overflow issues

--- workshop_portal/urls.py
+++ workshop_portal/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from workshop_portal import views
@@ -21,12 +21,12 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^workshop/', include('workshop_app.urls')),
-    url(r'^reset/', include('django.contrib.auth.urls')),
-    url(r'^page/', include('cms.urls')),
-    url(r'^statistics/', include('statistics_app.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.index),
+    re_path(r'^workshop/', include('workshop_app.urls')),
+    re_path(r'^reset/', include('django.contrib.auth.urls')),
+    re_path(r'^page/', include('cms.urls')),
+    re_path(r'^statistics/', include('statistics_app.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
